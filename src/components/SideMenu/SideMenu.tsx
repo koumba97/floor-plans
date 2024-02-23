703258617995ac05f8d7191ec9df18e32c84c5eb
Koumba KEITA <koumba.keita@epitech.eu>
2024-02-23 13:36:54 -0500

    Add SideMenu ui

---
+++ src/components/SideMenu/SideMenu.tsx
@@ -0,0 +1,25 @@
+import './SideMenu.scss';
+
+interface Props {
+    links: {
+        icon: string,
+        route: string,
+    }[]
+}
+const SideMenu = ({links}: Props) => {
+    return (
+        <div className='side-menu'>
+            <div className='list'>
+                {links.map( (link, index) => {
+                    return (
+                        <a href={link.route} key={index}>
+                            <i className={link.icon}></i>
+                        </a>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default SideMenu;
---
+++ src/data/MenuLinkData.ts
@@ -0,0 +1,36 @@
+import { MenuLink } from "../types/MenuLink";
+
+export const menuLinks: MenuLink[] = [
+    {
+        icon: 'las la-home',
+        route: '/'
+    },
+    {
+        icon: 'las la-users',
+        route: '/'
+    },
+    {
+        icon: 'las la-shapes',
+        route: '/'
+    },
+    {
+        icon: 'las la-city',
+        route: '/'
+    },
+    {
+        icon: 'las la-chart-pie',
+        route: '/'
+    },
+    {
+        icon: 'las la-comment-alt',
+        route: '/'
+    },
+    {
+        icon: 'las la-credit-card',
+        route: '/'
+    },
+    {
+        icon: 'las la-cog',
+        route: '/'
+    },
+]
